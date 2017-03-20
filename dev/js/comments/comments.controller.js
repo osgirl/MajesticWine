6b4a499ec4b6e7ccdfbf30ba1215f0d702a98edf
Paul Hume <paul@paulhume.co.uk>
2017-03-20 11:09:05 +0000

    Added functionality

---
+++ dev/js/comments/comments.controller.js
@@ -0,0 +1,42 @@
+(function () {
+    angular
+        .module('MajesticWine')
+        .controller('CommentsController', CommentsController);
+    function CommentsController() {
+        var vm = this;
+        vm.addReply = addReply;
+        vm.addComment = addComment;
+        vm.comments = [
+            {
+                name: 'Jimbo',
+                content: 'Bought this for first time and wouldn\'t buy again. Was not expecting the blush colour that came out and, for me, a little sweet. It is, nevertheless, a palatable drink for all that.',
+                date: '3 Days Ago',
+                replies: [
+                    {
+                        name: 'John Thomas',
+                        content: 'I\'m sorry to hear you didn\'t enjoy it Jimbo. It does need to be opened about an hour before drinking to git it some time to warm up to room temperature.',
+                        date: 'Yesterday'
+                    }
+                ]
+            },
+        ];
+        ////////////
+        function addReply(index) {
+            vm.comments[index].replies.push({
+                name: 'This User',
+                content: vm.reply[index],
+                date: 'Just now'
+            });
+            vm.reply[index] = undefined;
+        }
+        function addComment() {
+            vm.comments.push({
+                name: 'This User',
+                content: vm.comment,
+                date: 'Just now',
+                replies: []
+            });
+            vm.comment = undefined;
+        }
+    }
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,71 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var ts = require('gulp-typescript');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var ngAnnotate = require('gulp-ng-annotate');
 var csscomb = require('gulp-csscomb');
 var sass = require('gulp-sass');
+
+gulp.task('clean-scripts', function () {
+    return gulp.src([
+		'dev/js/**/*.*',
+		'dist/scripts/**/*.*'
+	], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('ts', ['clean-scripts'], function () {
+    return gulp.src('dev/ts/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            //out: 'output.js'
+        }))
+        .pipe(gulp.dest('dev/js'));
+});
+
+gulp.task('concat-scripts', ['ts'], function() {
+    return gulp.src([
+        'dev/js/main.js',
+        'dev/js/data.service.js',
+        'dev/js/authentication.js',
+        'dev/js/**/*.service.js',
+        'dev/js/**/*.directive.js',
+        'dev/js/**/*.controller.js'
+	])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('ngAnnotate', ['concat-scripts'], function() {
+    return gulp.src('dist/scripts/**/*.js')
+        .pipe(ngAnnotate())
+        .pipe(gulp.dest('dist/scripts'));
+})
+
+gulp.task('scripts', ['ngAnnotate'], function (cb) {
+	return gulp.src('dist/scripts/*.js')
+		.pipe(
+        	uglify()
+		)
+        .pipe(
+			gulp.dest('dist/scripts')
+		)
+});
+
+gulp.task('clean-html', function () {
+    return gulp.src([
+		'dist/**/*.html'
+	], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('html', ['clean-html'], function() {
+  return gulp.src('dev/html/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
 
 gulp.task('csscomb', function() {
     return gulp.src([
@@ -15,12 +80,14 @@ gulp.task('sass', ['csscomb'], function () {
         'dev/scss/**/*.scss',
         '!dev/scss/**/_*.scss'
     ])
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('dist/styles/'));
 });
 
 gulp.task('watch', function() {
+    gulp.watch('dev/ts/**/*.ts', ['scripts']);
+    gulp.watch('dev/html/**/*.html', ['html']);
     gulp.watch('dev/scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass'], function() {});
+gulp.task('default', ['scripts', 'html'], function() {});
